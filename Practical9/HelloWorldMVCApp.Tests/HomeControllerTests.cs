b1b6625019cae2726cf250feb4473f216255f9c8
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-04-23 20:37:05 +0530

    added comments to test methods

--- Practical9/HelloWorldMVCApp.Tests/HomeControllerTests.cs
+++ Practical9/HelloWorldMVCApp.Tests/HomeControllerTests.cs
@@ -6,6 +6,9 @@ namespace HelloWorldMVCApp.Tests
 {
     public class HomeControllerTests
     {
+        /// <summary>
+        /// This method verifies the Test3 action of HomeController when called returns a ViewResult.
+        /// </summary>
         [Fact]
         public void Test3_WhenCalled_ReturnsViewResult()
         {
@@ -19,7 +22,9 @@ namespace HelloWorldMVCApp.Tests
             Assert.IsType<ViewResult>(result);
         }
 
-
+        /// <summary>
+        /// This method verifies that the Test3 action sets the ViewBag.Message property to string "Hello World".
+        /// </summary>
         [Fact]
         public void Test3_WhenCalled_SetsHelloWorldMessage()
         {

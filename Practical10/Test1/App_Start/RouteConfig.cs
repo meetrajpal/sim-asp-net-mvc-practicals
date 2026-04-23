e85350760dccacacbdaf7933a5628edc2cf306f5
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-04-23 20:37:29 +0530

    implemented test 1 of practical 10

---
+++ Practical10/Test1/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Test1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Practical10/Test1/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    [RoutePrefix("employee-details")]
+    public class EmployeeController : Controller
+    {
+        [Route("~/")]
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{name}")]
+        public ActionResult Details(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}

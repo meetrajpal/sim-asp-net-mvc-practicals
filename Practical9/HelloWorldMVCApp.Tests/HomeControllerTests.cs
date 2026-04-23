e2442f2cba62e5dc33c5e675eed7871e01860e7a
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-04-23 19:08:13 +0530

    practical 9 implemented with individual functions in test 2

---
+++ Practical9/HelloWorldMVCApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using HelloWorldMVCApp.Controllers;
+using System.Web.Mvc;
+using Xunit;
+
+namespace HelloWorldMVCApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test3_WhenCalled_ReturnsViewResult()
+        {
+
+            var controller = new HomeController();
+
+
+            var result = controller.Test3();
+
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+
+        [Fact]
+        public void Test3_WhenCalled_SetsHelloWorldMessage()
+        {
+
+            var controller = new HomeController();
+
+
+            var result = controller.Test3() as ViewResult;
+
+
+            Assert.Equal("Hello World", result.ViewBag.Message);
+        }
+    }
+}
---
+++ Practical9/HelloWorldMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace HelloWorldMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+
+        public ActionResult Test3()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+    }
+}

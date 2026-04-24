44de1b1d864e91024f358839a3e676ae23d34d46
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-04-24 09:26:53 +0530

    handled errors and exceptions with error view

---
+++ Practical9/HelloWorldMVCApp/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace HelloWorldMVCApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Error";
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}

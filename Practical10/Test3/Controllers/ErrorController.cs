13a27f2b7effc0794376275103c7074a15c4085d
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-04-24 13:37:25 +0530

    test 3 implemented

---
+++ Practical10/Test3/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Test3.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Error";
+            return View("Error");
+        }
+    }
+}
---
+++ Practical10/Test3/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Test3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(CacheProfile = "CacheFor5Mins", Location = OutputCacheLocation.Client)]
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now;
+            return View();
+        }
+    }
+}
